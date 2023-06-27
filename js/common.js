ce7e77c5c6aa07edbd09ce1129fc23c26ac03f1a
PARK <lokkentech83@gmail.com>
2023-06-27 22:02:25 +0900

    반응형 디자인 공부 index화면

--- js/common.js
+++ js/common.js
@@ -1,1 +1,29 @@
-console.log("common.js의 로그");
+
+$(function() {
+
+  // 햄버거 버튼 클릭시
+  $( ".toggle-menu-button" ).on( "click", function() {
+    console.log("click");
+    console.log($(".header-site-menu").hasClass("is-show"));
+    if($(".header-site-menu").hasClass("is-show")) {
+      $(".header-site-menu").removeClass("is-show")
+    } else {
+      $(".header-site-menu").addClass("is-show")
+    }
+    
+  });
+})
+
+// toggle.js의 스크립트
+// window.addEventListener('load', function () {
+//   var $button = document.querySelector('.toggle-menu-button');
+//   var $menu = document.querySelector('.header-site-menu');
+//   $button.addEventListener('click', function () {
+//       if ($menu.classList.contains('is-show')) {
+//           $menu.classList.remove('is-show');
+//       }
+//       else {
+//           $menu.classList.add('is-show');
+//       }
+//   });
+// });
